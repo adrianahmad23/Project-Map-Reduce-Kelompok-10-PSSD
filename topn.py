f1a00818bfdd46f78d2f2b0321e62229ecdd001f
Adrian Ahmad Ardiansyah <adrian7281748@gmail.com>
2025-10-01 13:58:20 +0700

    Update topn.py

--- topn.py
+++ topn.py
@@ -13,3 +13,6 @@ df_sorted = df.sort_values(by="count", ascending=False)
 
 #tampilkan Top 20 kata
 print(df_sorted.head(20).to_csv(index=False))
+
+#simpan Top 20 ke CSV
+df_sorted.head(20).to_csv("top20_output.csv", index=False)
